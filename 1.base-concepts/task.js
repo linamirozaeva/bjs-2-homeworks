965758c78849d48410d16721f019fcea15b82593
karolina <karolinavasko@gmail.com>
2024-07-10 15:37:53 +0300

    доработка1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,48 +2,25 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = (b ** 2 - 4 * a * c);
+	let d = b ** 2 - 4 * a * c;
 	if (d > 0) {
-		let x1 = (-b + Math.sqrt(d)) / (2 * a);
-		let x2 = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(x1, x2);
-		console.log(arr);
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
 		return arr;
 	}
 	if (d === 0) {
-		let x0 = -b / (2 * a);
+		let root0 = -b / (2 * a);
 		arr.push(x0);
-		console.log(arr);
 		return arr;
-	} else {
-		console.log("корней нет" + arr)
 	}
 	return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-	let per = parseInt(percent) / 100 / 12;
-	let con = parseInt(contribution);
-	let am = parseInt(amount);
-	if (isNaN(per) || per < 0) {
-		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-	} else if (isNaN(con) || con < 0) {
-		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-	} else if (isNaN(am) || am < 0) {
-		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-	} else {
-
-		let today = new Date();
-		if (today.getFullYear() > date.getFullYear() || today.getMonth() > date.getMonth()) {
-			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
-		} else {
-			let s = am - con;
-			let n = date.getMonth() - today.getMonth() + (12 * (date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
-			let pay = s * (per + per / (((1 + per) ** n) - 1));
-			let totalAmount = (pay * n).toFixed(2);
-			console.log(totalAmount);
-			return +totalAmount;
-		}
-	}
-
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	let percentMonth = percent / 100 / 12;
+	let creditBody = amount - contribution;
+	let payment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+	let totalAmount = payment * countMonths;
+	return +totalAmount.toFixed(2); 
+  } 
