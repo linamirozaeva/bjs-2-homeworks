ab99a39fa14b389baa60ba2e460806ea708ded3b
karolina <karolinavasko@gmail.com>
2024-07-13 14:33:34 +0300

    бога нет.

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -53,14 +53,15 @@ function averageEvenElementsWorker(...arr){
   let countEvenElement = 0;
   if (arr.length === 0){
     return 0;
-  }
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] % 2 === 0){
-      sumEvenElement += arr[i];
-      countEvenElement += 1;
+  } else {
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 === 0){
+        sumEvenElement += arr[i];
+        countEvenElement += 1;
+      }
     }
+      return sumEvenElement / countEvenElement;
   }
-    return sumEvenElement / countEvenElement;
 }
 
 
