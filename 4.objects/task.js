bcdd2e7bb2aa94068b0f4e881a3298c5c67c6134
karolina <karolinavasko@gmail.com>
2024-07-19 12:46:14 +0300

    задание 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  }
+  Student.prototype.addMarks = function(...marksToAdd){
+    if (this.marks) {
+      this.marks.push(...marksToAdd);
+    }
+  }
+  Student.prototype.getAverage = function() {
+    if (this.marks) {
+      return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+    } else {
+      return 0;
+    }
+  }
+  Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
+  let student1 = new Student("Василиса", "женский", 19);
+  let student2 = new Student("Артем", "мужской", 25);
+  student1.setSubject("Algebra");
+  student1.addMarks(4, 5, 4, 2, 5);
+  student1.addMarks(5, 5, 5, 5);
+  student1.exclude('плохое учебное посещение');
+  console.log(student1);
+  console.log(student2);
